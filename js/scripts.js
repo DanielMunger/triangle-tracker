01b0e4cd77483f8409f10a440564a6a846e8fa2b
daniel munger and loren glenn <loren@>
2016-10-20 14:45:09 -0700

    add jquery to show/hide triangle types.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,57 @@
-var side1, side2, side3;
+var side1, side2, side3, equilateral, isosceles, scalene, notTriangle;
 
-if (side1 === side2 === side3)
+var calculate = function(side1, side2, side3){
+  if (!side1 || !side2 || !side3){
+    alert('Please insert a number for all 3 sides (not 0)')
+  }
 
-else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+  else if (side1 === side2 && side1 === side3){
+    equilateral();
+  }
 
-else if (side1 === side2 && side1 !== side3 && side1 + side2 > side3 || side2 === side3 && side2 !== side1 && side3 + side2 > side1 || side1 === side3 && side1 !== side2 && side3 + side1 > side2)
+  else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2){
+    scalene();
+  }
 
-else
+  else if (side1 === side2 && side1 !== side3 && side1 + side2 > side3 || side2 === side3 && side2 !== side1 && side3 + side2 > side1 || side1 === side3 && side1 !== side2 && side3 + side1 > side2){
+    isosceles();
+  }
+
+  else {
+    notTriangle()
+  };
+
+};
+//Front end logic
+
+$(document).ready(function(){
+
+  equilateral = function() {
+    $("#equilateral").show();
+    $("#isosceles, #scalene, #circle").hide();
+    $("#result").text("An equilateral triangle");
+  }
+  isosceles = function() {
+    $("#isosceles").show();
+    $("#equilateral, #scalene, #circle").hide();
+    $("#result").text("An isosceles triangle");
+  }
+  scalene = function() {
+    $("#scalene").show();
+    $("#isosceles, #equilateral, #circle").hide();
+    $("#result").text("A scalene triangle");
+  }
+  notTriangle = function() {
+    $("#circle").show();
+    $("#isosceles, #equilateral, #scalene").hide();
+    $("#result").text("Not a Triangle..");
+  }
+
+  $("#submit").click(function() {
+    side1 = parseInt($("#side1").val());
+    side2 = parseInt($("#side2").val());
+    side3 = parseInt($("#side3").val());
+    calculate(side1, side2, side3);
+  });
+
+});
