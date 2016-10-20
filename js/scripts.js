90db179ab451f78291411d18b5b4185cc8abb3fa
daniel munger and loren glenn <loren@>
2016-10-20 13:44:24 -0700

    add javascript checks for side lengths

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var side1, side2, side3;
+
+if (side1 === side2 === side3)
+
+else if (side1 !== side2 && side2 !== side3 && side1 !== side3 && side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+
+else if (side1 === side2 && side1 !== side3 && side1 + side2 > side3 || side2 === side3 && side2 !== side1 && side3 + side2 > side1 || side1 === side3 && side1 !== side2 && side3 + side1 > side2)
+
+else
